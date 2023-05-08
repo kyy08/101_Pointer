52ef204a920ec55d96ca18aeba1094e471b69705
kyy08 <kyyelzz1@gmail.com>
2023-05-08 13:47:30 +0700

    friendkelas

--- friendkelas/friendkelas.cpp
+++ friendkelas/friendkelas.cpp
@@ -24,8 +24,23 @@ public:
 
 void siswa::displayAll(orang& a) {
 	cout << id << endl << a.nama;
+}
 
-	void orang::setNama(string pNama) {
-		nama = pNama;
+void orang::setNama(string pNama) {
+	nama = pNama;
+}
+
+void siswa::setId(int pId) {
+	id = pId;
+}
+int main() {
+	orang joko;
+	joko.setNama("Joko Kumat");
+	siswa joko_siswa;
+	joko_siswa.setId(1);
+	joko_siswa.displayAll(joko);
+	return 0;
+}
+
 
 
